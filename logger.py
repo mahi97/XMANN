f87a7debaa37e84b86c96c646a0d157eb91eb6e3
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-11-28 18:34:14 +0330

    add logger

---
+++ logger.py
@@ -0,0 +1,8 @@
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def init_logging():
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] [%(name)s]  %(message)s',
+                        level=logging.DEBUG)
