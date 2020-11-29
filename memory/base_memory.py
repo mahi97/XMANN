e702e273ee99e782df7a7e5f1d35b65b581e2dc7
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-11-29 09:29:36 +0330

    add memory base and params

---
+++ memory/base_memory.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from attr import attrs, attrib, Factory
+
+
+class BaseMemory(nn.Module):
+    def __init__(self, args):
+        super(BaseMemory, self).__init__()
+
+        self.N = args.memory_size
+        self.M = args.word_size
+        self.init_mode = args.init_mode
+        self.batch_size = args.batch_size
+        self.is_cuda = args.is_cuda
+
+    def size(self):
+        return self.N, self.M
+
+    def reset(self):
+        raise NotImplementedError('Not Implemented in base class')
+
+    def read(self, address):
+        raise NotImplementedError('Not Implemented in base class')
+
+    def write(self, address, erase_vector, add_vector):
+        raise NotImplementedError('Not Implemented in base class')
+
+
+@attrs
+class MemoryParams(object):
+    name = attrib(default='base-memory')
+    memory_size = attrib(default=128, converter=int)
+    word_size = attrib(default=8, converter=int)
+    init_mode = attrib(default='const')
+    batch_size = attrib(default=1, converter=int)
+    is_cuda = attrib(default=False, converter=bool)
