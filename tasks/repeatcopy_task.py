a3208e6c2cd9c6874851c8b9bfacbc1687280bee
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-11-29 09:27:18 +0330

    update repeat copy

--- tasks/repeatcopy_task.py
+++ tasks/repeatcopy_task.py
@@ -8,26 +8,34 @@ import torch
 from torch import nn
 from torch import optim
 
+from network.networks import NETWORKS
+
+class RepeatCopyTask:
+    def __init__(self):
+        self.model = RepeatCopyTaskModel
+        self.param = RepeatCopyTaskParams
+
 
 # Generator of randomized test sequences
-def recopy_dataloader(num_batches, batch_size, seq_width, seq_min_len, seq_max_len, repeat_min, repeat_max, is_cuda=False):
+def data_loader(num_batches, batch_size, seq_width, seq_min_len, seq_max_len, repeat_min, repeat_max, is_cuda=False):
     """Generator of random sequences for the repeat copy task.
-  Creates random batches of "bits" sequences.
-  All the sequences within each batch have the same length.
-  The length is between `min_len` to `max_len`
-  :param num_batches: Total number of batches to generate.
-  :param batch_size: Batch size.
-  :param seq_width: The width of each item in the sequence.
-  :param seq_min_len: Sequence minimum length.
-  :param seq_max_len: Sequence maximum length.
-  :param repeat_min: Minimum repeatitions.
-  :param repeat_max: Maximum repeatitions.
-  NOTE: The input width is `seq_width + 2`. One additional input
-  is used for the delimiter, and one for the number of repetitions.
-  The output width is `seq_width` + 1, the additional input is used
-  by the network to generate an end-marker, so we can be sure the
-  network counted correctly.
-  """
+    Creates random batches of "bits" sequences.
+    All the sequences within each batch have the same length.
+    The length is between `min_len` to `max_len`
+    :param is_cuda: Use
+    :param num_batches: Total number of batches to generate.
+    :param batch_size: Batch size.
+    :param seq_width: The width of each item in the sequence.
+    :param seq_min_len: Sequence minimum length.
+    :param seq_max_len: Sequence maximum length.
+    :param repeat_min: Minimum repeatitions.
+    :param repeat_max: Maximum repeatitions.
+    NOTE: The input width is `seq_width + 2`. One additional input
+    is used for the delimiter, and one for the number of repetitions.
+    The output width is `seq_width` + 1, the additional input is used
+    by the network to generate an end-marker, so we can be sure the
+    network counted correctly.
+    """
     # Some normalization constants
     reps_mean = (repeat_max + repeat_min) / 2
     reps_var = (((repeat_max - repeat_min + 1) ** 2) - 1) / 12
@@ -67,6 +75,7 @@ def recopy_dataloader(num_batches, batch_size, seq_width, seq_min_len, seq_max_l
 @attrs
 class RepeatCopyTaskParams(object):
     name = attrib(default="recopy-task")
+    network = attrib(default='NTM', converter=str)
     controller_size = attrib(default=100, converter=int)
     controller_layers = attrib(default=1, converter=int)
     num_read_heads = attrib(default=1, converter=int)
@@ -83,33 +92,34 @@ class RepeatCopyTaskParams(object):
     rmsprop_lr = attrib(default=1e-4, converter=float)
     rmsprop_momentum = attrib(default=0.9, converter=float)
     rmsprop_alpha = attrib(default=0.95, converter=float)
+    is_cuda = attrib(default=False, converter=bool)
 
 
 @attrs
-class RepeatCopyTaskModelTraining(object):
+class RepeatCopyTaskModel(object):
     params = attrib(default=Factory(RepeatCopyTaskParams))
     net = attrib()
-    dataloader = attrib()
+    data_loader = attrib()
     criterion = attrib()
     optimizer = attrib()
 
     @net.default
     def default_net(self):
-        # See dataloader documentation
-        net = NTM(self.params.sequence_width + 2, self.params.sequence_width + 1,
+        # See data_loader documentation
+        net = NETWORKS[self.params.network](self.params.sequence_width + 2, self.params.sequence_width + 1,
                   self.params.controller_size, self.params.controller_layers,
                   self.params.num_read_heads, self.params.num_write_heads,
                   self.params.memory_n, self.params.memory_m)
-        if is_cuda:
+        if self.params.is_cuda:
             net = net.cuda()
         return net
 
-    @dataloader.default
-    def default_dataloader(self):
-        return recopy_dataloader(self.params.num_batches, self.params.batch_size,
-                                 self.params.sequence_width,
-                                 self.params.sequence_min_len, self.params.sequence_max_len,
-                                 self.params.repeat_min, self.params.repeat_max)
+    @data_loader.default
+    def default_data_loader(self):
+        return data_loader(self.params.num_batches, self.params.batch_size,
+                           self.params.sequence_width,
+                           self.params.sequence_min_len, self.params.sequence_max_len,
+                           self.params.repeat_min, self.params.repeat_max)
 
     @criterion.default
     def default_criterion(self):
