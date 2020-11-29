2e7cc310bad509e0a48c1629b7730973c22e6ade
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-11-29 20:47:16 +0330

    add abstract model to solve all sort of models

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from controller.controllers import *
+from data_path.data_paths import *
 from memory.memories import *
 from head.heads import *
 
@@ -9,11 +10,12 @@ from head.base_head import HeadsParams
 from memory.base_memory import MemoryParams
 from controller.base_controller import ControllerParams
 
+from data_path.base_data_path import *
 
 from attr import attrs, attrib, Factory
 
 
-class BaseNetwork(nn.Module):
+class Model(nn.Module):
 
     def __init__(self, args):
         """Initialize an NTM.
@@ -25,7 +27,7 @@ class BaseNetwork(nn.Module):
         :param N: Number of rows in the memory bank.
         :param M: Number of cols/features in the memory bank.
         """
-        super(BaseNetwork, self).__init__()
+        super(Model, self).__init__()
 
         self.num_inputs = args.num_inputs
         self.num_outputs = args.num_outputs
@@ -35,6 +37,8 @@ class BaseNetwork(nn.Module):
         self.num_write_heads = args.num_write_heads
         self.N = args.memory_size
         self.M = args.word_size
+        self.is_cuda = args.is_cuda
+        self.batch_size = args.batch_size
 
         memory_param = MemoryParams(memory_size=args.memory_size,
                                     word_size=args.word_size,
@@ -53,20 +57,42 @@ class BaseNetwork(nn.Module):
         self.heads = nn.ModuleList([HEADS[args.read_head](head_params) for _ in range(args.num_read_heads)])
         self.heads += [HEADS[args.write_head](head_params) for _ in range(args.num_write_heads)]
 
-        self.data_path = DataPath(num_inputs, num_outputs, controller, memory, heads)
+        data_path_params = DataPathParams(num_inputs=self.num_inputs,
+                                          num_outputs=self.num_outputs,
+                                          controller=self.controller,
+                                          memory=self.memory,
+                                          heads=self.heads,
+                                          is_cuda=args.is_cuda)
+        self.data_path = DATA_PATHS[args.data_path](data_path_params)
+
+        self.previous_state = None
+
     def calculate_num_params(self):
         """Returns the total number of parameters."""
         num_params = 0
         for p in self.parameters():
             num_params += p.data.view(-1).size(0)
-
         return num_params
+
+    def init_sequence(self):
+        """Initializing the state."""
+        self.memory.reset()
+        self.previous_state = self.data_path.create_new_state(self.batch_size)
+
+    def forward(self, x=None):
+        if x is None:
+            x = torch.zeros(self.batch_size, self.num_inputs)
+        if self.is_cuda:
+            x = x.cuda()
+        o, self.previous_state = self.data_path(x, self.previous_state)
+        return o, self.previous_state
 
 
 @attrs
-class NetworkParams(object):
+class ModelParams(object):
     memory = attrib(default='static')
     controller = attrib(default='LSTM')
+    data_path = attrib(default='NTM')
     num_inputs = attrib(default=-1, converter=int)
     num_outputs = attrib(default=-1, converter=int)
     num_hidden = attrib(default=-1, converter=int)
