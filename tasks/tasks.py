4a656a5fad43680dbad68b3839e760e318e776c8
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-12-31 21:06:40 +0900

    add new tasks

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,10 +1,12 @@
 from tasks import copy_task
 from tasks import repeatcopy_task
 from tasks import associativerecall_task
+from tasks import priority_sort_task
 
 TASKS = {
     'copy': copy_task.CopyTask(),
     'repeat-copy': repeatcopy_task.RepeatCopyTask(),
-    'associative-recall': associativerecall_task.AssociativeRecallTask()
+    'associative-recall': associativerecall_task.AssociativeRecallTask(),
+    'priority-sort': priority_sort_task.PrioritySortTask()
 }
 
