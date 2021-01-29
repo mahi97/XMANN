7e8ed638f7c2613e534f12bbb15eeb277f69dd0a
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-01-29 20:14:43 +0900

    fix imports

--- head/static_head.py
+++ head/static_head.py
@@ -1,10 +1,4 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-from attr import attrs, attrib, Factory
-
-from head.base_head import BaseHead
+from head.base_head import *
 
 
 class StaticHead(BaseHead):
@@ -74,13 +68,3 @@ class StaticHead(BaseHead):
         if self.is_cuda:
             result = result.cuda()
         return result
-
-
-def split_cols(mat, lengths):
-    """Split a 2D matrix to variable length columns."""
-    assert mat.size()[1] == sum(lengths), "Lengths must be summed to num columns"
-    l = np.cumsum([0] + lengths)
-    results = []
-    for s, e in zip(l[:-1], l[1:]):
-        results += [mat[:, s:e]]
-    return results
