6440c6b4d89bec8af8b216e79595fc0e9fd7f192
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-01-29 20:14:25 +0900

    add dynamic heads to heads

--- head/heads.py
+++ head/heads.py
@@ -1,7 +1,11 @@
 from head.static_read_head import StaticReadHead
 from head.static_write_head import StaticWriteHead
+from head.dynamic_write_head import DynamicWriteHead
+from head.dynamic_read_head import DynamicReadHead
 
 HEADS = {
     'static-read': StaticReadHead,
-    'static-write': StaticWriteHead
+    'static-write': StaticWriteHead,
+    'dynamic-read': DynamicReadHead,
+    'dynamic-write': DynamicWriteHead
 }
