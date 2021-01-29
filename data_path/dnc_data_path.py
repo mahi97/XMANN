5e7daa594a4c8f7d0b6ad08e9ff0e9ee681a7fed
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-01-29 20:13:14 +0900

    just a copy of NTM

--- data_path/dnc_data_path.py
+++ data_path/dnc_data_path.py
@@ -4,4 +4,51 @@ from data_path.base_data_path import *
 
 
 class DNC(BaseDataPath):
-    pass
+    def __init__(self, args):
+        super(DNC, self).__init__(args)
+
+    def forward(self, x, prev_state):
+        """DataPath forward function.
+        :param x: input vector (batch_size x num_inputs)
+        :param prev_state: The previous state of the DataPath
+        """
+        # Unpack the previous state
+        prev_reads, prev_controller_state, prev_heads_states = prev_state
+
+        # Use the controller to get an embeddings
+        if self.is_cuda:
+            x = x.cuda()
+            for i in range(len(prev_reads)):
+                prev_reads[i] = prev_reads[i].cuda()
+
+        inp = torch.cat([x] + prev_reads, dim=1)
+        if self.is_cuda:
+            inp = inp.cuda()
+        controller_out, controller_state = self.controller(inp, prev_controller_state)
+
+        # Read/Write from the list of heads
+        reads = []
+        heads_states = []
+        for head, prev_head_state in zip(self.heads, prev_heads_states):
+            if head.is_read_head():
+                r, head_state = head(controller_out, prev_head_state)
+                if self.is_cuda:
+                    r = r.cuda()
+                    head_state = head_state.cuda()
+                reads += [r]
+            else:
+                head_state = head(controller_out, prev_head_state)
+                if self.is_cuda:
+                    head_state = head_state.cuda()
+            heads_states += [head_state]
+
+        # Generate Output
+        inp2 = torch.cat([controller_out] + reads, dim=1)
+        o = torch.sigmoid(self.fc(inp2))
+        # o = torch.sigmoid(self.fc(controller_out))
+
+        # Pack the current state
+        state = (reads, controller_state, heads_states)
+
+        return o, state
+
