12719d86fd0e1c52d58f612ae9051a5514c06d17
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-01-29 20:13:39 +0900

    fix addressing and read modes

--- head/dynamic_head.py
+++ head/dynamic_head.py
@@ -1,10 +1,4 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-from attr import attrs, attrib, Factory
-
-from head.base_head import BaseHead
+from head.base_head import *
 
 
 class DynamicHead(BaseHead):
@@ -24,18 +18,33 @@ class DynamicHead(BaseHead):
         return NotImplementedError
 
 
-def split_cols(mat, lengths):
-    """Split a 2D matrix to variable length columns."""
-    assert mat.size()[1] == sum(lengths), "Lengths must be summed to num columns"
-    l = np.cumsum([0] + lengths)
-    results = []
-    for s, e in zip(l[:-1], l[1:]):
-        results += [mat[:, s:e]]
-    return results
+def content_address(self, key_vector, key_strength):
+    """NTM Addressing (according to section 3.3).
+    Returns a softmax weighting over the rows of the memory matrix.
+    :param key_vector: The key vector.
+    :param key_strength: The key strength (focus).
+    """
+    wc = F.softmax(key_strength * F.cosine_similarity(key_vector.unsqueeze(1), self.memory.memory, dim=2), dim=1)
+    return wc
 
 
-@attrs
-class HeadsParams(object):
-    controller = attrib(default=None)
-    memory = attrib(default=None)
-    is_cuda = attrib(default=False, converter=bool)
+def location_address(self, wc, gate, shift, sharpen, last_address):
+    wg = (gate * wc) + (1 - gate) * last_address
+    ws = self.convolve(wg, shift, self.batch_size)
+    ws = (ws ** sharpen)
+    wt = torch.true_divide(ws, torch.sum(ws, dim=1).view(-1, 1) + 1e-16)
+
+    return wt
+
+
+def address(self, key_vector, key_strength):
+    """NTM Addressing (according to section 3.3).
+    Returns a softmax weighting over the rows of the memory matrix.
+    :param key_vector: The key vector.
+    :param key_strength: The key strength (focus).
+    """
+    # Handle Activations
+    k = torch.tanh(key_vector)
+    B = F.softplus(key_strength)
+    wc = self.content_address(k, B)
+    return wc
