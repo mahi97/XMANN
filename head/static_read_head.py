d8e518ed11816e1c00303e9607847a6b76ea8ad9
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-01-29 16:48:50 +0900

    fix multi head reading and new addressing structure

--- head/static_read_head.py
+++ head/static_read_head.py
@@ -1,13 +1,14 @@
-from head.base_head import *
+from head.static_head import *
 
 
-class StaticReadHead(BaseHead):
+class StaticReadHead(StaticHead):
     def __init__(self, args):
         super(StaticReadHead, self).__init__(args)
 
         # Corresponding to k, β, g, s, γ sizes from the paper
-        self.read_vector = [self.M, 1, 1, 3, 1]
-        self.fc_read = nn.Linear(self.ctrl_size, sum(self.read_vector))
+        self.vector = [self.M, 1, 1, 3, 1]
+        self.read_vector = self.vector * self.num_read_heads
+        self.fc_read = nn.Linear(self.ctrl_size, sum(self.vector))
         self.reset_parameters()
 
     def create_new_state(self, batch_size):
@@ -23,9 +24,11 @@ class StaticReadHead(BaseHead):
     def is_read_head(self):
         return True
 
-    def forward(self, input, last_w):
-        out = self.fc_read(input)
-        K, B, G, S, L = split_cols(out, self.read_vector)
-        w = self._address_memory(K, B, G, S, L, last_w)
+    def forward(self, x, last_w):
+        out = self.fc_read(x)
+        size = len(self.vector)
+        segment = self.id * size
+        K, B, G, S, L = split_cols(out, self.read_vector[segment: segment + size])
+        w = self.address(K, B, G, S, L, last_w)
         r = self.memory.read(w)
         return r, w
